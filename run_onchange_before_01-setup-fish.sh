2be345d4e019c637eb1c8a1a45f1f78772aec556
Kevin Nguyen <ngynkvn@gmail.com>
2024-08-29 06:06:51 -0400

    fix: Need to debug nvm

--- run_onchange_before_01-setup-fish.sh
+++ run_onchange_before_01-setup-fish.sh
@@ -2,9 +2,9 @@
 
 type fisher &>/dev/null || curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
 
-fisher install jorgebucaran/nvm.fish
-fisher install IlanCosman/tide@v6
-fisher install kidonng/zoxide.fish
+fisher install jorgebucaran/nvm.fish \
+  IlanCosman/tide@v6 \
+  kidonng/zoxide.fish
 
 tide configure --auto --style=Lean --prompt_colors='True color' \
   --show_time='24-hour format' --lean_prompt_height='Two lines' \
@@ -12,7 +12,3 @@ tide configure --auto --style=Lean --prompt_colors='True color' \
   --prompt_spacing=Sparse --icons='Few icons' --transient=No
 
 set -U zoxide_cmd cd
-
-set NODE_VERSION v20.16.0
-nvm install $NODE_VERSION
-set -U nvm_default_version $NODE_VERSION
