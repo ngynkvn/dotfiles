87dc3b1aec4292270750e0b7ecb7bcbc6e340c54
Kevin <ngynkvn@gmail.com>
2024-08-29 07:46:48 -0400

    fix: Remove fish_variables and sync cat

--- private_dot_config/private_fish/functions/cat.fish
+++ private_dot_config/private_fish/functions/cat.fish
@@ -1,4 +1,4 @@
 set _EXE (basename (type -p batcat || type -p bat))
-function cat --wraps=$E --description "alias cat=$_EXE"
+function cat --wraps=$E --description 'alias cat=$E'
     $_EXE $argv
 end
