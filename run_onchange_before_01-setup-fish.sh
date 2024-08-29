0c76d0d633b040fc268a3a7687c4baeb2dc76947
Kevin Nguyen <ngynkvn@gmail.com>
2024-08-29 04:57:39 -0400

    fix: tide config

---
+++ run_onchange_before_01-setup-fish.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/fish
+
+type fisher &>/dev/null || curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
+
+fisher install jorgebucaran/nvm.fish
+fisher install IlanCosman/tide@v6
+
+tide configure --auto --style=Lean --prompt_colors='True color' \
+  --show_time='24-hour format' --lean_prompt_height='Two lines' \
+  --prompt_connection=Solid --prompt_connection_andor_frame_color=Darkest \
+  --prompt_spacing=Sparse --icons='Few icons' --transient=No
