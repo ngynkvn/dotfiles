f24a954bb2e0c1347ea9807cb1573748f57ad1c5
Kevin <ngynkvn@gmail.com>
2024-09-21 22:39:25 -0400

    feat: Add models and prompt functionality to llama.cpp
      * Add models directory to .chezmoiignore to prevent them from being committed
      * Add .keep file to mark directories that should not be committed
      * Add functionality to prompt the user for a commit message and commit the changes

--- private_dot_config/private_fish/functions/llmdiffsum.fish
+++ private_dot_config/private_fish/functions/llmdiffsum.fish
@@ -1,13 +1,13 @@
 function llmdiffsum
     set -l ignored
-    cat .gitattributes | grep 'binary$' | cut -f1 -d' ' | while read line
+    /bin/cat .gitattributes | grep 'binary$' | cut -f1 -d' ' | while read line
         set -a ignored ":/!$line"
     end
     echo "ignoring: $ignored"
-    begin
-        echo 'Summarize my diff in a commit message for me:'
-        echo '<diff>'
-        git diff HEAD -- $ignored | /bin/cat
-        echo '</diff>'
-    end | ollama run llama3.1
+    set prompt "
+Summarize my diff in a commit message for me:
+<diff>
+$(git diff HEAD -- $ignored | /bin/cat)
+</diff>
+"; and aiprompt $prompt
 end
