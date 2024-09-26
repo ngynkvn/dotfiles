827dd77cfd046e96c5dc2c9be7b6d76d5f2d4d87
Kevin <ngynkvn@gmail.com>
2024-09-25 22:14:13 -0400

    feat: update llama-cli function to accept additional arguments for grammar and context size.

--- private_dot_config/private_fish/functions/lldiff.fish
+++ private_dot_config/private_fish/functions/lldiff.fish
@@ -1,5 +1,5 @@
 function lldiff
-    argparse 'g/grammar=?' 'm/model_name=?' 'd/model_dir=?' 'l/n_gpu_layers=?' 't/n_tokens=?' s/select -- $argv; or return
+    argparse 'g/grammar=?' 'm/model_name=?' 'd/model_dir=?' 't/n_tokens=?' 'c/ctx_size=?' s/select -- $argv; or return
     type -q llama-cli; or begin
         echo "Missing llama-cli in path. Exiting"
         return 1
@@ -7,8 +7,8 @@ function lldiff
     set -q _flag_g; and set GRAMMAR $_flag_g; or set GRAMMAR "$HOME/.local/llama.cpp/grammars/conventional-commit.gbnf"
     set -q _flag_m; and set MODEL_NAME $_flag_m; or set MODEL_NAME 'llama-3.2-3b-instruct-q8_0.gguf'
     set -q _flag_d; and set MODEL_DIR $_flag_d; or set MODEL_DIR "$HOME/.local/llama.cpp/models/"
-    set -q _flag_l; and set NGPU_LAYERS $_flag_ng; or set NGPU_LAYERS 80
-    set -q _flag_t; and set NTOKENS $_flag_nt; or set NTOKENS 512
+    set -q _flag_c; and set CTXSIZE $_flag_c; or set CTXSIZE 8192
+    set -q _flag_t; and set NTOKENS $_flag_t; or set NTOKENS 512
 
     set -q _flag_s
     and set MODEL_PATH (find gguf $MODEL_DIR | fzf )
@@ -21,12 +21,10 @@ function lldiff
     echo "ignoring: $ignored"
 
     set system_prompt "\
-You are a commit message generator. 
-Write short commit messages and include details based on the
-diff file that follows. 
-Think carefully before you write your commit message.
-The message should not be any longer than 240 characters.
-What you write will be given to `git commit -m [message]`.
+You are a helpful assistant that generates commit messages. 
+Write short commit messages that includes details based on the diff output that the user provides 
+The message should only be one sentence that captures as much detail as possible.
+Write ONLY the commit message, and nothing else.
 Examples:
     - feat: implement JWT-based authentication system for secure user sessions
     - feat: add command line parsing and subcommands
@@ -37,15 +35,17 @@ Examples:
 $(git diff HEAD -- $ignored | /bin/cat)
 </diff>"
     set promptfile (mktemp)
-    _mk_prompt $MODEL_NAME $system_prompt $user_prompt >$promptfile
+    and _mk_prompt $MODEL_NAME $system_prompt $user_prompt >$promptfile
     and begin
         set_color 5261a9
-        llama-cli -m $MODEL_PATH \
-            -f $promptfile \
-            -fa -ngl $NGPU_LAYERS -n $NTOKENS \
+        llama-cli \
             --no-display-prompt \
-            #--log-disable \
+            -m $MODEL_PATH \
+            -f $promptfile \
+            -fa -n $NTOKENS -c $CTXSIZE \
             --grammar-file $GRAMMAR
+        #--log-disable \
         set_color normal
+        rm $promptfile
     end
 end
