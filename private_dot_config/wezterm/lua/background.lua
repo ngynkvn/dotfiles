cd7af7e352601a94792a49232fbb11e89dbb9526
Kevin <ngynkvn@gmail.com>
2024-10-07 09:29:42 -0400

    background: bluer

--- private_dot_config/wezterm/lua/background.lua
+++ private_dot_config/wezterm/lua/background.lua
@@ -2,13 +2,13 @@ local wezterm = require("wezterm")
 
 return {
 	{
-		source = { Color = "Black" },
+		source = { Color = "00111a" },
 		width = "100%",
 		height = "100%",
 	},
 	{
 		source = { File = wezterm.home_dir .. "/.local/share/background/noise-2.png" },
-		hsb = { brightness = 0.25 },
+		hsb = { brightness = 0.2 },
 		opacity = 0.4,
 		height = 700,
 		width = 700,
