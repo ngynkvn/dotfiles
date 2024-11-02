fcedf6a4ced8e6edb9ae7a8c5bb4cf38eb003955
Kevin <ngynkvn@gmail.com>
2024-11-02 07:05:53 -0400

    betterkeys

--- private_dot_config/wezterm/lua/keys.lua
+++ private_dot_config/wezterm/lua/keys.lua
@@ -13,19 +13,23 @@ local DisabledDefaults = {
 
 function M.setup(config)
 	config.keys = {
-		{ key = "\\", mods = "ALT", action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
-		{ key = "-", mods = "ALT", action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }) },
-		{ key = "w", mods = "ALT", action = wezterm.action.ActivatePaneDirection("Up") },
-		{ key = "a", mods = "ALT", action = wezterm.action.ActivatePaneDirection("Left") },
-		{ key = "s", mods = "ALT", action = wezterm.action.ActivatePaneDirection("Down") },
-		{ key = "d", mods = "ALT", action = wezterm.action.ActivatePaneDirection("Right") },
-		{ key = "w", mods = "ALT|SHIFT", action = wezterm.action.AdjustPaneSize({ "Up", 1 }) },
-		{ key = "a", mods = "ALT|SHIFT", action = wezterm.action.AdjustPaneSize({ "Left", 1 }) },
-		{ key = "s", mods = "ALT|SHIFT", action = wezterm.action.AdjustPaneSize({ "Down", 1 }) },
-		{ key = "d", mods = "ALT|SHIFT", action = wezterm.action.AdjustPaneSize({ "Right", 1 }) },
+		{ key = "+", mods = "CMD|SHIFT", action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
+		{ key = "-", mods = "CMD|SHIFT", action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }) },
+		--{ key = "\\", mods = "CMD|SHIFT", action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
+
+		{ key = "h", mods = "ALT", action = wezterm.action.ActivatePaneDirection("Left") },
+		{ key = "j", mods = "ALT", action = wezterm.action.ActivatePaneDirection("Down") },
+		{ key = "k", mods = "ALT", action = wezterm.action.ActivatePaneDirection("Up") },
+		{ key = "l", mods = "ALT", action = wezterm.action.ActivatePaneDirection("Right") },
+
+		{ key = "h", mods = "ALT|SHIFT", action = wezterm.action.AdjustPaneSize({ "Left", 1 }) },
+		{ key = "j", mods = "ALT|SHIFT", action = wezterm.action.AdjustPaneSize({ "Down", 1 }) },
+		{ key = "k", mods = "ALT|SHIFT", action = wezterm.action.AdjustPaneSize({ "Up", 1 }) },
+		{ key = "l", mods = "ALT|SHIFT", action = wezterm.action.AdjustPaneSize({ "Right", 1 }) },
 
 		{ key = "h", mods = "CMD", action = wezterm.action.ActivateTabRelative(-1) },
 		{ key = "l", mods = "CMD", action = wezterm.action.ActivateTabRelative(1) },
+
 		{ key = "h", mods = "CMD|SHIFT", action = wezterm.action.MoveTabRelative(-1) },
 		{ key = "l", mods = "CMD|SHIFT", action = wezterm.action.MoveTabRelative(1) },
 
