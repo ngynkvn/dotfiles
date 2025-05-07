e9e9d1b1178316aade2eb1a2dc0519ef55f25c91
Kevin Nguyen <ngynkvn@gmail.com>
2025-05-07 02:30:20 -0400

    fix

--- private_dot_config/private_fish/conf.d/abbrs.fish
+++ private_dot_config/private_fish/conf.d/abbrs.fish
@@ -1,11 +1,11 @@
-abbr -a -- zr 'zoxide remove' # imported from a universal variable, see `help abbr`
-abbr -a -- cdqi 'zoxide query -i' # imported from a universal variable, see `help abbr`
-abbr -a -- zq 'zoxide query' # imported from a universal variable, see `help abbr`
-abbr -a -- cdq 'zoxide query' # imported from a universal variable, see `help abbr`
-abbr -a -- cda 'zoxide add' # imported from a universal variable, see `help abbr`
-abbr -a -- cdr 'zoxide remove' # imported from a universal variable, see `help abbr`
-abbr -a -- za 'zoxide add' # imported from a universal variable, see `help abbr`
-abbr -a -- zqi 'zoxide query -i' # imported from a universal variable, see `help abbr`
+abbr -a -- zr 'zoxide remove'
+abbr -a -- cdqi 'zoxide query -i'
+abbr -a -- zq 'zoxide query'
+abbr -a -- cdq 'zoxide query'
+abbr -a -- cda 'zoxide add'
+abbr -a -- cdr 'zoxide remove'
+abbr -a -- za 'zoxide add'
+abbr -a -- zqi 'zoxide query -i'
 abbr -a --position anywhere --function last_history_item -- !!
 abbr -a -- zbr 'zig build run'
 abbr -a -- zb 'zig build'
@@ -14,3 +14,5 @@ abbr -a -- cb 'cargo build'
 abbr -a -- gr 'go run '
 abbr -a -- gm 'go mod tidy'
 abbr -a -- fzfp "fzf --preview 'bat --color=always {}' --preview-window '~3'"
+abbr -a -- jl 'jj log'
+abbr -a -- jjbsr 'jj b s -r \'@-\''
